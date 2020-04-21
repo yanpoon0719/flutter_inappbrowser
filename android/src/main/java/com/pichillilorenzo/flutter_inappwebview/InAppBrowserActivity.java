aa0a7c41423372e911873fd9da38daeffb93d288
Mok Ka Chun <mkc188@users.noreply.github.com>
2020-04-21 12:32:50 +0800

    Try fix

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppBrowserActivity.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/InAppBrowserActivity.java
@@ -50,6 +50,17 @@ public class InAppBrowserActivity extends AppCompatActivity {
   public boolean isHidden = false;
   public String fromActivity;
 
+  private String getProcessName(Context context) {
+    if (context == null) return null;
+    ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
+      if (processInfo.pid == android.os.Process.myPid()) {
+        return processInfo.processName;
+      }
+    }
+    return null;
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -73,6 +84,14 @@ public class InAppBrowserActivity extends AppCompatActivity {
     webView.options = webViewOptions;
 
     if (InAppWebViewFlutterPlugin.inAppBrowser == null || InAppWebViewFlutterPlugin.inAppBrowser.webViewActivities == null) return;
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        String processName = getProcessName(this);
+        String packageName = this.getPackageName();
+        if (!packageName.equals(processName)) {
+            WebView.setDataDirectorySuffix(processName);
+        }
+    }
 
     InAppWebViewFlutterPlugin.inAppBrowser.webViewActivities.put(uuid, this);
 
